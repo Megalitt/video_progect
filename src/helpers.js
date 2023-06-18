1d93d282ad7fc63f3788ab1d62c499e14d848aed
Андрей Шастин <97834468+Megalitt@users.noreply.github.com>
2023-06-18 20:20:27 +0300

    Delete src directory

--- src/helpers.js
+++
@@ -1,6 +0,0 @@
-export function millisecondConverter(millis) {
-  let minutes = Math.floor(millis / 60000);
-  let seconds= Math.floor((millis % 60000) / 1000).toFixed(0);
-  let miliSeconds = ((millis % 60000) % 1000);
-  return (minutes < 10 ? '0' : '') + minutes + ":" + (seconds < 10 ? '0' : '') + seconds + ":" + (miliSeconds < 10 ? '0' : '') + miliSeconds;
-};
